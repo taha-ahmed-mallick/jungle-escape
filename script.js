0ac63e53a952d00ac2a6bca6e98e5821ffb190e5
Taha Ahmed Mallick <tahaahmedmallick@gmail.com>
2023-07-19 23:12:36 +0500

    Made all the decoration stuff.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let strtBtn = document.getElementsByClassName("strt-btn")[0];
 let canvas = document.getElementsByTagName("canvas")[0];
 
 let ctx = canvas.getContext("2d");
-let a, plankInfo, aspectRatio;
+let a;
 
 function resize() {
       canvas.width = window.innerWidth;
@@ -18,7 +18,6 @@ window.addEventListener('resize', resize);
 setTimeout(() => screenTransition(screens[0], screens[1]), /*3000*/1);
 
 function screenTransition(screenR, screenE) {
-      console.log(screenR, screenE);
       screenR.style.opacity = 0;
       screenE.classList.remove("hidden");
       screenE.style.opacity = 1;
@@ -34,24 +33,42 @@ strtBtn.addEventListener("click", () => screenTransition(screens[1], screens[2])
 let woodPlank = new Image();
 woodPlank.src = './wood-plank-light.png';
 
+let tree = new Image();
+tree.src = "./trees.png";
+
 function drawDecorations() {
-      ctx.fillStyle = "#03A9F4";
+      ctx.fillStyle = "#2E7D32";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.fillStyle = "#de993a";
-      ctx.fillRect(canvas.width * 0.2, 0, canvas.width * 0.6, canvas.height);
+      drawTrees();
       drawBridge();
 }
 
+function drawTrees() {
+      let treeInfo = {
+            aspectRatio: tree.naturalHeight / tree.naturalWidth,
+            width: 400,
+            height: null,
+            treeX: null,
+            treeY: null
+      };
+      treeInfo.height = treeInfo.width * treeInfo.aspectRatio;
+      treeInfo.treeX = Math.ceil(canvas.width / treeInfo.width);
+      treeInfo.treeY = Math.ceil(canvas.height / treeInfo.height);
+      for (let i = 0; i < treeInfo.treeY; i++) {
+            for (let j = 0; j < treeInfo.treeX; j++) ctx.drawImage(tree, treeInfo.width * j, treeInfo.height * i, treeInfo.width, treeInfo.height);
+      }
+}
+
 function drawBridge() {
-      aspectRatio = woodPlank.naturalWidth / woodPlank.naturalHeight;
-      plankInfo = {
+      let plankInfo = {
+            aspectRatio: woodPlank.naturalWidth / woodPlank.naturalHeight,
             height: canvas.height / 25,
             width: null,
             planksY: 25,
             planksX: [],
             ttlPlanks: null
       };
-      plankInfo.width = aspectRatio * plankInfo.height;
+      plankInfo.width = plankInfo.aspectRatio * plankInfo.height;
       plankInfo.planksX[0] = Math.floor((canvas.width * 0.6) / plankInfo.width);
       plankInfo.planksX[1] = (((canvas.width * 0.6) / plankInfo.width) - Math.floor((canvas.width * 0.6) / plankInfo.width)) * plankInfo.width;
       plankInfo.ttlPlanks = (plankInfo.planksX[1] == 0 ? plankInfo.planksX[0] : plankInfo.planksX[0] + 1);
